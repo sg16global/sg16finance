83b3f89d1cb7e31fea1f61bc35600b930c517978
SG16 Global <sg16global@gmail.com>
2026-08-03 19:00:00 +0800

    Fix homepage desktop grid layout broken by flex override

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="dashboard-canvas page-flow mx-auto max-w-[1440px] px-2 pt-1 pb-6 sm:px-3 lg:px-5 lg:pb-8">
       {/* Mobile: map first, then ads + cards stack. Desktop: 3-column grid */}
-      <div className="home-shield-stack grid items-start gap-3 sm:gap-4 lg:grid-cols-[minmax(0,280px)_1fr_minmax(0,280px)] xl:grid-cols-[minmax(0,300px)_1fr_minmax(0,300px)] xl:gap-4">
+      <div className="flex flex-col items-stretch gap-3 sm:gap-4 lg:grid lg:items-start lg:grid-cols-[minmax(0,280px)_1fr_minmax(0,280px)] xl:grid-cols-[minmax(0,300px)_1fr_minmax(0,300px)] xl:gap-4">
         <div className="order-1 min-w-0 lg:order-2 lg:col-span-1">
           <WorldMap />
         </div>
